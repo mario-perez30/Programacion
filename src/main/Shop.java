4d1d1ec8d2cf463698765207d34f19e390207ee5
mario_perez30 <mperezma709@gmail.com>
2025-01-09 09:44:24 +0100

    Carga Inicial

---
+++ src/main/Shop.java
@@ -0,0 +1,308 @@
+/*
+#Issue 1: Show cash option does not show the value 
+      -Resuelto agragando el valor cash el cual se encarga de guardar el valor que hay en la caja.
+
+#Issue 2: Show inventory does not show details data for each product 
+
+#Issue 3: Add product allows to have duplicated products
+       -Resuelto now <add product> don't allow the sames products
+
+#Issue 4: Option "10. Salir programa" does not exit program 
+       -Resuelto we only need to change the while for: while diferent of true
+
+#Issue 5: Option to add stock replaces value but add it to current value
+       -Resuelto we can call the function getstock for get the current stock and then we can sum this stock with the neewstock that we have
+        
+#Issue 6: Option "7. Ver ventas" goes error
+
+#Issue 7: Public price is not set when a new product is created
+
+#Issue 8: Public price is not updated when a product is set as expired
+
+#Enhancement 9: Create a new option 8 to show total amount of all sales
+
+#Enhancement 10: Use Amount data type 
+*/
+
+
+
+
+package main;
+
+import model.Product;
+import model.Sale;
+import java.util.Scanner;
+
+public class Shop {
+
+    private double cash = 100.00;
+    private Product[] inventory;
+    private int numberProducts;
+    private Sale[] sales;
+
+    final static double TAX_RATE = 1.04;
+
+    public Shop() {
+        inventory = new Product[10];
+    }
+
+    public static void main(String[] args) {
+        Shop shop = new Shop();
+
+        shop.loadInventory();
+
+        Scanner scanner = new Scanner(System.in);
+        int opcion = 0;
+        boolean exit = false;
+
+        do {
+            System.out.println("\n");
+            System.out.println("===========================");
+            System.out.println("Menu principal miTienda.com");
+            System.out.println("===========================");
+            System.out.println("1) Contar caja");
+            System.out.println("2) Agregar producto");
+            System.out.println("3) Agregar stock");
+            System.out.println("4) Marcar producto proxima caducidad");
+            System.out.println("5) Ver inventario");
+            System.out.println("6) Venta");
+            System.out.println("7) Ver ventas");
+            System.out.println("9) Salir programa");
+            System.out.print("Seleccione una opcion: ");
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    shop.showCash();
+                    break;
+
+                case 2:
+                    shop.addProduct();
+                    break;
+
+                case 3:
+                    shop.addStock();
+                    break;
+
+                case 4:
+                    shop.setExpired();
+                    break;
+
+                case 5:
+                    shop.showInventory();
+                    break;
+
+                case 6:
+                    shop.sale();
+                    break;
+
+                case 7:
+                    shop.showSales();
+                    break;
+
+                case 8:
+                    exit = true;
+                    break;
+            }
+        } while (exit!=true);
+    }
+
+    /**
+     * load initial inventory to shop
+     */
+    public void loadInventory() {
+        addProduct(new Product("Manzana", 10.00, true, 10));
+        addProduct(new Product("Pera", 20.00, true, 20));
+        addProduct(new Product("Hamburguesa", 30.00, true, 30));
+        addProduct(new Product("Fresa", 5.00, true, 20));
+    }
+
+    /**
+     * show current total cash
+     */
+    private void showCash() {
+        System.out.println("Dinero actual: " + cash);  //error solucionado 
+    }
+
+    /**
+     * add a new product to inventory getting data from console
+     */
+    public void addProduct() {
+        if (isInventoryFull()) {
+            System.out.println("No se pueden anadir mas productos");
+            return;
+        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nombre: ");
+        String name = scanner.nextLine();
+        Product product = findProduct(name);
+
+        if (product != null) {
+                System.out.print("Producto ya existente en nuestra tienda ");
+            }
+            else{
+                System.out.print("Precio mayorista: ");
+        double wholesalerPrice = scanner.nextDouble();
+        System.out.print("Stock: ");
+        int stock = scanner.nextInt();
+
+        addProduct(new Product(name, wholesalerPrice, true, stock));
+                
+            }
+            
+        }
+        
+    
+
+    /**
+     * add stock for a specific product
+     */
+    public void addStock() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Seleccione un nombre de producto: ");
+        String name = scanner.next();
+        Product product = findProduct(name);
+
+        if (product != null) {
+            // ask for stock
+            System.out.print("Seleccione la cantidad a anadir: ");
+            int newstock = scanner.nextInt();
+            // update stock product
+            product.setStock(product.getStock() + newstock);
+            System.out.println("El stock del producto " + name + " ha sido actualizado a " + product.getStock());
+
+        } else {
+            System.out.println("No se ha encontrado el producto con nombre " + name);
+        }
+    }
+
+    /**
+     * set a product as expired
+     */
+    private void setExpired() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Seleccione un nombre de producto: ");
+        String name = scanner.next();
+
+        Product product = findProduct(name);
+
+        if (product != null) {
+            System.out.println("El stock del producto " + name + " ha sido actualizado a " + product.getPublicPrice());
+
+        }
+    }
+
+    /**
+     * show all inventory
+     */
+    public void showInventory() {
+        System.out.println("Contenido actual de la tienda:");
+        for (Product product : inventory) {
+            if (product != null) {
+                System.out.println(product);
+            }
+        }
+    }
+
+    /**
+     * make a sale of products to a client
+     */
+    public void sale() {
+        // ask for client name
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Realizar venta, escribir nombre cliente");
+        String client = sc.nextLine();
+
+        // sale product until input name is not 0
+        double totalAmount = 0.0;
+        String name = "";
+        while (!name.equals("0")) {
+            System.out.println("Introduce el nombre del producto, escribir 0 para terminar:");
+            name = sc.nextLine();
+
+            if (name.equals("0")) {
+                break;
+            }
+            Product product = findProduct(name);
+            boolean productAvailable = false;
+
+            if (product != null && product.isAvailable()) {
+                productAvailable = true;
+                totalAmount += product.getPublicPrice();
+                product.setStock(product.getStock() - 1);
+                // if no more stock, set as not available to sale
+                if (product.getStock() == 0) {
+                    product.setAvailable(false);
+                }
+                System.out.println("Producto añadido con éxito");
+            }
+
+            if (!productAvailable) {
+                System.out.println("Producto no encontrado o sin stock");
+            }
+            
+        }
+
+        // show cost total
+        totalAmount = totalAmount * TAX_RATE;
+        cash += totalAmount;
+        System.out.println("Venta realizada con éxito, total: " + totalAmount);
+        Sale sale = new Sale(client, inventory, totalAmount);
+        
+    }
+
+    /**
+     * show all sales
+     */
+    private void showSales() {
+        System.out.println("Lista de ventas:");
+        for (Sale sale : sales) {
+            if (sale != null) {
+                System.out.println(sale);
+            }
+        }
+    }
+
+    /**
+     * add a product to inventory
+     *
+     * @param product
+     */
+    public void addProduct(Product product) {
+        if (isInventoryFull()) {
+            System.out.println("No se pueden anadir mas productos, se ha alcanzado el maximo de " + inventory.length);
+            return;
+        }
+        inventory[numberProducts] = product;
+        numberProducts++;
+    }
+
+    /**
+     * check if inventory is full or not
+     *
+     * @return true if inventory is full
+     */
+    public boolean isInventoryFull() {
+        if (numberProducts == 10) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * find product by name
+     *
+     * @param name
+     * @return product found by name
+     */
+    public Product findProduct(String name) {
+        for (int i = 0; i < inventory.length; i++) {
+            if (inventory[i] != null && inventory[i].getName().equals(name)) {
+                return inventory[i];
+            }
+        }
+        return null;
+    }
+
+}
